6b2cc290c6a66f06aee39a319ef0fd5677978b6d
AdrianS2002 <aschiop2002@yahoo.com>
2024-11-28 19:19:44 +0200

    signals implemented

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../../../public/dummy-users';
 
 
@@ -11,13 +11,14 @@ console.log(randomIndex);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
-
-  get imagePath() {
-    return `./users/` + this.selectedUser.avatar ;
-  }
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() =>'./users/' + this.selectedUser().avatar);
+  // get imagePath() {
+  //   return `./users/` + this.selectedUser.avatar ;
+  // }
 
   onSelectUser(){
-    this.selectedUser = DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)];
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
